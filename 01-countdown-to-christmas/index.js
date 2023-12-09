9db892ec7b0b7df79c4392944eed74744a7890e4
Katerina Volakova <volakova.kat@gmail.com>
2023-12-09 13:05:49 +0100

    days, minutes and seconds countdown

--- 01-countdown-to-christmas/index.js
+++ 01-countdown-to-christmas/index.js
@@ -1,6 +1,7 @@
-const countdownDisplay = document.querySelector("#countdown-display")
+const countdownDisplay = document.querySelector("#countdown-display");
+const countdownDisplayFull = document.querySelector("#countdown-display-full");
 
-function renderCountdown(){
+const renderCountdown = () => {
     let christmas = 24
     // Task:
     // - Get today's date (you only need the day):
@@ -11,8 +12,38 @@ function renderCountdown(){
     countdownDisplay.textContent = difference;
 }
  
-renderCountdown()
+renderCountdown();
 
 // Stretch goals:
 // - Display hours, minutes, seconds.
+function renderCountdownFull(){
+  let christmas = new Date("Dec 23, 2023 23:59:59").getTime();
+
+  setInterval(() => {
+    let now = new Date().getTime();
+    let difference = christmas - now;
+
+    let days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+    const formatNumber = (number) => {
+      return number < 10 ? `0${number}` : number;
+    }
+
+    days = formatNumber(days);
+    hours = formatNumber(hours);
+    minutes = formatNumber(minutes);
+    seconds = formatNumber(seconds);
+
+
+    countdownDisplayFull.textContent =  `${days} : ${hours} : ${minutes} : ${seconds}`;
+    
+  }, 1000);
+
+}
+
+renderCountdownFull();
+
 // - Add a countdown for another festival, your birthday, or Christmas 2022.
